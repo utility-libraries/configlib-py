fbb698410203093fd4b3a32ae9ffa9e7029ebfde
PlayerG9 <g9yout@gmail.com>
2023-09-19 09:58:03 +0200

    added basic tests

---
+++ tests/test_loaders.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+import unittest
+
+
+class TestLoaders(unittest.TestCase):
+    def test_conf(self):
+        from configlib import autoload
+
+        config = autoload("./test_configs/app.conf")
+        self.assertIsNotNone(config)
+
+    def test_json(self):
+        from configlib import autoload
+
+        config = autoload("./test_configs/app.json")
+        self.assertIsNotNone(config)
+
+    def test_jsonc(self):
+        from configlib import autoload
+
+        config = autoload("./test_configs/app.jsonc")
+        self.assertIsNotNone(config)
+
+    def test_json5(self):
+        from configlib import autoload
+
+        config = autoload("./test_configs/app.json5")
+        self.assertIsNotNone(config)
+
+    def test_toml(self):
+        from configlib import autoload
+
+        config = autoload("./test_configs/app.toml")
+        self.assertIsNotNone(config)
+
+    def test_xml(self):
+        from configlib import autoload
+
+        config = autoload("./test_configs/app.xml")
+        self.assertIsNotNone(config)
+
+    def test_yaml(self):
+        from configlib import autoload
+
+        config = autoload("./test_configs/app.yaml")
+        self.assertIsNotNone(config)
+
+
+if __name__ == '__main__':
+    unittest.main()

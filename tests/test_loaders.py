10cbea4bbff1011c5baad9a4caa9e78768ccd662
PlayerG9 <g9yout@gmail.com>
2023-11-01 10:42:24 +0100

    adjusted tests for ConfigInterface

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -15,43 +15,48 @@ class TestLoaders(unittest.TestCase):
         from configlib import autoload
 
         config = autoload(HERE / "test_configs" / "app.conf")
-        self.assertIsNotNone(config)
+        self.assertEqual(config.getstr('owner', 'name'), "John Doe")
+        self.assertEqual(config.getint('database', 'port'), 143)
 
     def test_json(self):
         from configlib import autoload
 
         config = autoload(HERE / "test_configs" / "app.json")
-        self.assertIsNotNone(config)
+        self.assertEqual(config.getstr('first_name'), "John")
+        self.assertEqual(config.getstr('phone_numbers', 0, 'type'), "home")
 
     def test_jsonc(self):
         from configlib import autoload
 
         config = autoload(HERE / "test_configs" / "app.jsonc")
-        self.assertIsNotNone(config)
+        self.assertFalse(config.getboolean('true'))
+        self.assertEqual(config.getint('number'), 42)
 
     def test_json5(self):
         from configlib import autoload
 
         config = autoload(HERE / "test_configs" / "app.json5")
-        self.assertIsNotNone(config)
+        self.assertEqual(config.get('positiveSign'), 1)
 
     def test_toml(self):
         from configlib import autoload
 
         config = autoload(HERE / "test_configs" / "app.toml")
-        self.assertIsNotNone(config)
+        self.assertEqual(config.getstr('title'), "TOML Example")
+        self.assertTrue(config.getboolean('database', 'enabled'))
 
     def test_xml(self):
         from configlib import autoload
 
         config = autoload(HERE / "test_configs" / "app.xml")
-        self.assertIsNotNone(config)
+        self.assertEqual(config.getstr('message', 'warning', '#'), "Hello World")
 
     def test_yaml(self):
         from configlib import autoload
 
         config = autoload(HERE / "test_configs" / "app.yaml")
-        self.assertIsNotNone(config)
+        self.assertEqual(config.getstr('date'), "2012-08-06")
+        self.assertEqual(config.getint('items', 1, 'quantity'), 1)
 
 
 if __name__ == '__main__':

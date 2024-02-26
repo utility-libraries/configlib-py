3a93af6484a4ec5a742072fde75b53e488285272
PlayerG9 <g9yout@gmail.com>
2024-02-26 09:04:28 +0100

    added path variations

---
+++ src/configlib/finder/_variations.py
@@ -0,0 +1,30 @@
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+import re
+import itertools
+import typing as t
+from ..loader import get_supported_formats
+
+
+PATTERN = re.compile(r"{(.+?)}")
+WILDCARD_EXT = re.compile(r"\.ext$")
+ALL_EXTENSIONS = f".{{{','.join(get_supported_formats())}}}"
+
+
+def variations(*variants: str) -> t.Iterator[str]:
+    r"""
+    variations("hello.{yaml,yml}") -> ["hello.yaml", "hello.yml"]
+    variations("hello.ext") -> ["hello.ini", "hello.conf", "hello.json", ...]
+    """
+    for variant in variants:
+        variant = WILDCARD_EXT.sub(ALL_EXTENSIONS, variant)  # "hello.ext" => "hello.{ini,conf,json,...}"
+        patterns: t.List[str] = PATTERN.findall(variant)
+        if not patterns:
+            yield variant
+            continue
+        patterns: t.List[t.List[str]] = [pattern.split(",") for pattern in patterns]
+        template = PATTERN.sub('{}', variant)
+        for values in itertools.product(*patterns):
+            yield template.format(*values)
--- src/configlib/finder/find.py
+++ src/configlib/finder/find.py
@@ -5,7 +5,7 @@ r"""
 """
 from pathlib import Path
 from ..exceptions import ConfigNotFoundError
-from ..loader import get_supported_formats
+from ._variations import variations as iter_variations
 from .default_places import DEFAULT_PLACES, T_PLACES
 
 
@@ -15,6 +15,10 @@ def find(*variants: str, places: T_PLACES = None) -> Path:
 
     note: if a variant ends with .ext then every supported config-type is searched for
 
+    note: you can make variations with file.{ext1,ext2}
+
+    note: absolute paths have higher priority than relative
+
     :param variants: name-variants of the config file
     :param places: list of directories to search in
     :return: pathlib.Path
@@ -23,28 +27,36 @@ def find(*variants: str, places: T_PLACES = None) -> Path:
     if places is None:
         places = DEFAULT_PLACES
 
-    # Todo. Split variants by absolute or not
+    variations = list(iter_variations(*variants))
 
+    # split by absolute or relative
+    absolutes, relatives = [], []
+
+    for variation in map(Path, variations):
+        if variation.is_absolute():
+            absolutes.append(variation)
+        else:
+            relatives.append(variation)
+
+    # check absolutes
+    for fp in absolutes:
+        if fp.is_file():
+            return fp
+
+    # check relatives
     for place in places:
         if callable(place):
             place = place()
-        if place is None:
+        if place is None:  # not-supported on this platform
             continue
 
         place = Path(place)
-        if not place.is_dir():
+        if not place.is_dir():  # doesn't exist. No need to continue searching
             continue
 
-        for variant in variants:
-            fp = place / variant
-            if fp.suffix == ".ext":
-                for extension in get_supported_formats():
-                    fpx = fp.with_suffix(f".{extension}")
-                    if fpx.is_file():
-                        return fpx
-            else:
-                if fp.is_file():
-                    return fp
+        for variation in relatives:
+            fp = place / variation
+            if fp.is_file():
+                return fp
 
-    # Error Message
     raise ConfigNotFoundError("no config-file could be found anywhere")

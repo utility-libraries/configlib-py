a8ff97ba044e1fa53196a80fddffeb5d0944a9a4
PlayerG9 <g9yout@gmail.com>
2024-03-22 11:34:28 +0100

    improved NotFoundError in .loading.loaders

--- src/configlib/loading/loaders/__init__.py
+++ src/configlib/loading/loaders/__init__.py
@@ -3,23 +3,23 @@
 r"""
 
 """
-from configlib.exceptions import NotSupportedError
+from configlib.exceptions import NotSupportedError as __NotSupportedError
 from .conf_loader import load_conf
 from .xml_loader import load_xml
 from .json_loader import load_json
 from .jsonc_loader import load_jsonc
 try:
     from .json5_loader import load_json5
-except NotSupportedError:
+except __NotSupportedError:
     def load_json5(*_, **__):
-        raise NotSupportedError()
+        raise __NotSupportedError("json5-support is not installed")
 try:
     from .toml_loader import load_toml
-except NotSupportedError:
+except __NotSupportedError:
     def load_toml(*_, **__):
-        raise NotSupportedError()
+        raise __NotSupportedError("toml-support is not installed")
 try:
     from .yaml_loader import load_yaml
-except NotSupportedError:
+except __NotSupportedError:
     def load_yaml(*_, **__):
-        raise NotSupportedError()
+        raise __NotSupportedError("yaml-support is not installed")
